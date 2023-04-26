114c9a1942f8f2572446467a0f7a7e6caeecc679
Brian Faust <hello@basecode.sh>
2023-04-26 04:01:55 +0300

    first commit
    Signed-off-by: Brian Faust <hello@basecode.sh>

---
+++ src/ServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BombenProdukt\BladeIcons\GovIcons;
+
+use BombenProdukt\BladeIcons\Facades\IconFamilyRegistry;
+use BombenProdukt\BladeIcons\IconFamily;
+use BombenProdukt\PackagePowerPack\Package\AbstractServiceProvider;
+
+final class ServiceProvider extends AbstractServiceProvider
+{
+    public function packageRegistered(): void
+    {
+        IconFamilyRegistry::push(
+            IconFamily::fromDirectory('govicons', __DIR__ . '/../resources/svg'),
+        );
+    }
+}
